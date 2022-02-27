039b2b31b404df0d1ccee133da952bc965d50fc2
zhujie <1421634618@qq.com>
2022-02-27 15:07:37 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const db = require("./models/db");
+
+const users = require("./routes/api/users")
+
+app.get("/",(req,res) => {
+    res.send("Hello World!");
+})
+
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,() => {
+    console.log(`Server is running on port ${port}`);
+})
